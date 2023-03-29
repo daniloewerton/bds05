63bbd2fd0c2456d1482e9442a844328b864b2aff
Danilo Ewerton <danilo.ewe@gmail.com>
2023-03-29 20:14:51 -0300

    Implementando entidades

---
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -0,0 +1,79 @@
+package com.devsuperior.movieflix.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private List<Role> roles = new ArrayList<>();
+    private List<Review> reviews = new ArrayList<>();
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
